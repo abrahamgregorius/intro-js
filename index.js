c7920b55212ea911c02a4a3d8741c7e89f9eff00
Abraham <athioii6@gmail.com>
2024-08-10 10:32:32 +0700

    init

---
+++ index.js
@@ -0,0 +1,144 @@
+// Variables
+let variablePertama = "Hello world";
+var variableKedua = "Hello";
+const variableKetiga = "World";
+
+// Casing in naming variables
+let PascalCase = "";
+let snake_case = "";
+// let kebab-case = ""
+let camelCase = "";
+
+// Data types
+let nama = "Alfian";
+let umur = 16;
+let isMarried = false;
+const pi = 3.14;
+
+// Operators
+// Arithmetic
+// +
+// -
+// /
+// *
+// %
+
+// Assignment
+let number = 12;
+number += 10;
+number = number + 10; // reassignment
+
+number -= 1;
+number /= 1;
+number *= 1;
+
+// Comparison
+// >
+// <
+// <=
+// >=
+// ==
+
+// Functions
+// function sayHello(nama="Hendri") {
+//     console.log('Hello World ' + nama)
+// }
+
+// sayHello()
+
+// function penjumlahan(x, y) {
+//     let total = x + y
+//     return total
+// }
+
+// let hasil = penjumlahan(1, 1)
+// console.log(hasil)
+
+// const perkalian = (x, y) => {
+//     return x * y
+// }
+
+// console.log(perkalian(1,2))
+
+// Objects
+let orangPertama = {
+    firstName: "Joni",
+    lastName: "Suharman",
+    age: 19,
+    isMarried: true,
+};
+
+// console.log(orangPertama.firstName + " " + orangPertama.lastName)
+// console.log("Is "+ orangPertama.firstName + " married?")
+// console.log(orangPertama.isMarried)
+
+let orangBanyak = [
+    {
+        nama: "Alfi",
+        age: 17,
+        isMarried: false,
+    },
+    {
+        nama: "Arthur",
+        age: 17,
+        isMarried: false,
+    },
+    {
+        nama: "Hendri",
+        age: 17,
+        isMarried: false,
+    },
+];
+
+// iterations
+// for(let i = 100; i > 10; i--) {
+//     console.log(i)
+// }
+
+// i++ increment
+// let j = 5
+// for(let i = 0; j < 10; i++) {
+//     console.log(i )
+// }
+
+// let i = 0
+// while(true) {
+//     if(i < 10) {
+//         console.log(i)
+//         i++
+//     } else {
+//         break
+//     }
+// }
+
+let numbers = [12, 23, 34, 54, 63, 12, 283];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+// for (let i = numbers.length - 1; i >= 0; i--) {
+//     console.log(numbers[i]);
+// }
+
+// numbers.forEach((i) => {
+//     console.log(i)
+// })
+
+
+// let people = ["joni", "budi", "tedi", "dendi", "adi"] // people.length = 5
+// let age = [14, 15, 16, 12, 23 ] // age.length = 5
+
+// people.forEach((a, i) => {
+//     console.log("Nama saya " + a + ". Umur saya adalah " + age[i])
+// })
+
+
+// Casting
+// let amount = "12.2"
+// let amount2 = "13.5"
+// let hasil = parseFloat(amount) + parseFloat(amount2)
+
+// console.log(amount + amount2)
+// console.log(hasil)
+// console.log(typeof(hasil))
